68c96bc21f7ad0880cca58f56cd49adc8e324909
AndriyChmykhal <chmykhal@ukr.net>
2020-03-26 00:52:33 +0200

    ip extractor

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,15 +1,25 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
 public class RegExp {
 
-  /**
-   * Get IP address from text.
-   * @param text text to parse
-   * @return String with IP address
-   */
-  public static String getIpAddress(String text) {
-    // TODO fill in code here for extracting IP address using Regular Expression
-    return "";
-  }
+    /**
+     * Get IP address from text.
+     *
+     * @param text text to parse
+     * @return String with IP address
+     */
+    public static String getIpAddress(String text) {
+        Pattern pattern = Pattern.compile("[0-2]?[0-9]?[0-9][.][0-2]?[0-9]?[0-9][.][0-2]?[0-9]?[0-9][.][0-2]?[0-9]?[0-9]");
+        Matcher matcher = pattern.matcher(text);
+        String ipAdress = "";
+        if (matcher.find()) {
+            ipAdress = matcher.group(0);
+        }
+        System.out.println(ipAdress);
+        return ipAdress;
+    }
 }
